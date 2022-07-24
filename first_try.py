cfb5775b376d327cf9706637e9868e9b627eb003
Balalina <105857927+Balalina@users.noreply.github.com>
2022-07-24 21:15:44 +0400

    it's alive!

--- first_try.py
+++ first_try.py
@@ -1,12 +1,10 @@
 from selenium.webdriver.chrome import webdriver
 import time
 
-driver = webdriver.WebDriver(executable_path="chromedriver.exe")
+driver = webdriver.WebDriver(executable_path='chromedriver.exe')
 
-driver.get("http://habr.com")
+driver.get('https://habr.com')
 
 time.sleep(5)
 
 driver.quit()
-
-
