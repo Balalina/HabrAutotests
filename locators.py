aff688e7620ab7cf4a866bcc8dae2ae436bebdca
Balalina <105857927+Balalina@users.noreply.github.com>
2022-09-26 16:21:35 +0400

    move locators, add new test

---
+++ locators.py
@@ -0,0 +1,9 @@
+from selenium.webdriver.common.by import By
+
+
+last_page_locator = By.XPATH, '(//*[@class="tm-pagination__page"])[last()]'
+article_locator = By.TAG_NAME, 'article'
+search_icon_locator = By.CSS_SELECTOR, 'span.tm-search__icon'
+search_input_locator = By.CLASS_NAME, 'tm-input-text-decorated__input'
+search_button_locator = By.CLASS_NAME, 'tm-header-user-menu__search'
+empty_res_locator = By.CLASS_NAME, 'tm-empty-placeholder__text'
